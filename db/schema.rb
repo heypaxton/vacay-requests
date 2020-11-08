898f24ac61d7c34c3df01eb51cb5bd4b2f8d9b2b
heypaxton <heypaxton@gmail.com>
2020-11-08 11:08:42 -0800

    Add first name, last name and type to user

--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,9 @@ ActiveRecord::Schema.define(version: 2020_11_08_190135) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
