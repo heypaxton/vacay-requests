cbab540c683c66173d1f55c4e13c45b6dd4418e4
heypaxton <heypaxton@gmail.com>
2020-11-08 12:08:51 -0800

    Add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do |worker|
+  User.create!(first_name: 'test', last_name: "user #{worker}", email: "testworker#{worker}@test.com", password: "password", password_confirmation: "password", worker_id: "xyz2020")
+end
+
+1.times do |manager|
+  ManagerUser.create!(first_name: 'test', last_name: "user #{manager}", email: "testmananger#{manager}@test.com", password: "password", password_confirmation: "password", worker_id: "xyz2021")
+end
+
+10.times do |request|
+  Request.create!(status: 'pending', vacation_start_date: Date.tomorrow, vacation_end_date: Date.tomorrow + 5.days, author: "#{User.first.first_name}  #{User.first.last_name}", user_id: 1)
+end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Request, type: :model do
   before do
     user = User.create(first_name: 'test', last_name: 'user', email: "testworker@test.com", password: "password", password_confirmation: "password", worker_id: "xyz2020")
-    @request = Request.create(status: 'pening', vacation_start_date: Date.tomorrow, vacation_end_date: Date.tomorrow + 5.days, author: "#{user.first_name}  #{user.last_name}", user_id: user.id)
+    @request = Request.create(status: 'pending', vacation_start_date: Date.tomorrow, vacation_end_date: Date.tomorrow + 5.days, author: "#{user.first_name}  #{user.last_name}", user_id: user.id)
   end
   describe "creating a request" do
     it 'it can be created' do
