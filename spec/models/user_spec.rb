17cce622aadc889f40b68384d0dd854fd8c437a5
heypaxton <heypaxton@gmail.com>
2020-11-08 11:12:33 -0800

    Add user creation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
+  describe "creating a user" do
+    before do
+      @user = User.create(first_name: 'test', last_name: 'user', email: "testworker@test.com", password: "password", password_confirmation: "password")
+    end
+
+    it "can be created" do
+      expect(@user).to be_valid 
+    end
+  end
 end
